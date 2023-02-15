318f9b609ee968014fea80727f2df1d1d3c2dec6
Sanskar-Sarawgi <120471589+Sanskar-Sarawgi@users.noreply.github.com>
2023-02-15 19:07:52 +0530

    Add files via upload

--- store_user.rb
+++ store_user.rb
@@ -1,16 +1,31 @@
-require 'mysql'
+require 'mysql2'
 
 
-begin
-    con = Mysql.new 'localhost', 'user12', '34klq*'
-    puts con.get_server_info
-    rs = con.query 'SELECT VERSION()'
-    puts rs.fetch_row    
-    
-rescue Mysql::Error => e
-    puts e.errno
-    puts e.error
-    
-ensure
-    con.close if con
+Client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => '2001')
+Client.query("use db1");
+#create table if not exit
+
+def show()
+    results = Client.query("select * from user_info");
+    results.each do |row|
+        puts row
+    end
 end
+
+def insert(name,age,place)
+    results = Client.query("select * from user_info")
+    id = results.size+1;
+    Client.query("insert into user_info value(#{id},#{age},\"#{name}\",\"#{place}\")")
+end
+
+def delete(id)
+    Client.query("DELETE FROM user_info WHERE userid=#{id}")
+end
+
+show
+insert("random",3,"xyz")
+show
+delete(1)
+
+
+
